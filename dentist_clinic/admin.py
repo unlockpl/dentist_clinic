fc8c867e7854530021a4c6cc7f50b814807f31b7
unlockpl <apiszczkiewicz@interia.pl>
2021-10-14 17:40:43 +0200

    models and migration

--- dentist_clinic/admin.py
+++ dentist_clinic/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from dentist_clinic.models import Appointment, PatientHistory, Procedure, Room, UserData
 # Register your models here.
--- dentist_clinic/models.py
+++ dentist_clinic/models.py
@@ -1,3 +1,33 @@
+import django.contrib.auth.models
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Procedure(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    doctors = models.ManyToManyField(User)
+
+
+class Appointment(models.Model):
+    date = models.DateTimeField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    procedure = models.ForeignKey(Procedure, on_delete=models.CASCADE)
+
+
+class PatientHistory(models.Model):
+    description = models.TextField()
+
+
+class UserData(models.Model):
+    phone = models.CharField(max_length=32)
+    address = models.TextField()
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    appointments = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    patient_histories = models.ForeignKey(PatientHistory, on_delete=models.CASCADE)
