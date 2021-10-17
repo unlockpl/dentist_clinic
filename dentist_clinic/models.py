a02da764c4c68befe9baef2e0c083697d8245eb7
unlockpl <apiszczkiewicz@interia.pl>
2021-10-17 14:06:32 +0200

    model fix, appointment lists done

--- dentist_clinic/models.py
+++ dentist_clinic/models.py
@@ -18,16 +18,18 @@ class Procedure(models.Model):
 class Appointment(models.Model):
     date = models.DateTimeField()
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient_appointment', null=True)
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='doctor_appointment', null=True)
     procedure = models.ForeignKey(Procedure, on_delete=models.CASCADE)
 
 
 class PatientHistory(models.Model):
     description = models.TextField()
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient_history', null=True)
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='doctor_history', null=True)
 
 
 class UserData(models.Model):
     phone = models.CharField(max_length=32)
     address = models.TextField()
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    appointments = models.ForeignKey(Appointment, on_delete=models.CASCADE, blank=True, null=True)
-    patient_histories = models.ManyToManyField(PatientHistory, blank=True)
--- dentist_clinic/views.py
+++ dentist_clinic/views.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView, DeleteView, DetailView, FormView, ListView, UpdateView
 
@@ -10,9 +12,14 @@ from dentist_clinic.models import Appointment, PatientHistory, Procedure, Room, 
 
 # Create your views here.
 
+class DoctorUserMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='Doctor').exists()
 
-def doctor_login_check(user):
-    return user.groups.filter(name='Doctor').exists()
+
+class PatientUserMixin(LoginRequiredMixin, UserPassesTestMixin):
+    def test_func(self):
+        return self.request.user.groups.filter(name='Patient').exists()
 
 
 class HomeView(View):
@@ -43,43 +50,78 @@ class UserDataFormView(View):
     pass
 
 
-class DoctorListView(ListView):
+class AppointmentFormView(View):
+    pass
+
+
+class DoctorListView(DoctorUserMixin, ListView):
     model = UserData
     template_name = 'lists/doctor-list.html'
     paginate_by = 50
 
     def get_queryset(self):
+        print(UserData.objects.filter(user__groups=Group.objects.filter(name='Doctor').first()).order_by('user__last_name'))
         return UserData.objects.filter(user__groups=Group.objects.filter(name='Doctor').first())\
             .order_by('user__last_name')
 
 
-class PatientListView(ListView):
+class PatientListView(DoctorUserMixin, ListView):
     model = UserData
     template_name = 'lists/patient-list.html'
     paginate_by = 50
 
-    def get_queryset(self):
-        return UserData.objects.filter(user__groups=Group.objects.filter(name='Patient').first())\
-            .order_by('user__last_name')
+    # def get_queryset(self):
+    #     return UserData.objects.filter(user__groups=Group.objects.filter(name='Patient').first())\
+    #         .order_by('-user__last_name')
 
 
-class RoomListView(ListView):
+class RoomListView(DoctorUserMixin, ListView):
     model = Room
     template_name = 'lists/room-list.html'
     paginate_by = 50
 
 
-class AppointmentListByPatientView(View):
-    pass
+class AppointmentListByPatientView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    model = Appointment
+    template_name = 'lists/appointment-list-by-patient.html'
+    paginate_by = 50
+
+    def get_queryset(self):
+        return Appointment.objects.filter(patient__pk=self.kwargs['pk']).order_by('-date')
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Doctor').exists() or self.request.user.pk == self.kwargs['pk']
 
 
-class AppointmentListByDoctorView(View):
-    pass
+class AppointmentListByDoctorView(DoctorUserMixin, ListView):
+    model = Appointment
+    template_name = 'lists/appointment-list-by-doctor.html'
+    paginate_by = 50
+
+    def get_queryset(self):
+        return Appointment.objects.filter(doctor__pk=self.kwargs['pk']).order_by('-date')
 
 
-class AppointmentListByRoomView(View):
-    pass
+class AppointmentListByRoomView(ListView):
+    model = Appointment
+    template_name = 'lists/appointment-list-by-room.html'
+    paginate_by = 50
+
+    def get_queryset(self):
+        return Appointment.objects.filter(room__pk=self.kwargs['pk']).order_by('-date')
 
 
-class AppointmentFormView(View):
-    pass
+class AppointmentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    def test_func(self):
+        appointment = Appointment.objects.get(pk=self.kwargs['pk'])
+        return self.request.user.groups.filter(name='Doctor').exists() or self.request.user == appointment.patient
+
+
+class AppointmentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Appointment
+    success_url = reverse_lazy('home')
+    template_name = 'home.html'
+
+    def test_func(self):
+        appointment = Appointment.objects.get(pk=self.kwargs['pk'])
+        return self.request.user.groups.filter(name='Doctor').exists() or self.request.user == appointment.patient
